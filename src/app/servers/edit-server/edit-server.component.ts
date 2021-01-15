2a16aa1b9e0c347a40edb235f2ae1c2b7dfc3278
jfspps <jfsapps@gmail.com>
2021-01-15 17:31:11 +0000

    retrieving queryParams and fragment data

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,9 +13,21 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+    private currentRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // recall that this component is accessed via /servers/id/edit
+    // both queryParams and fragment have subscribe() methods available which respond to changes with data and
+    // enable editServerComponent to get get updated when the user is already viewing editServerComponent
+    console.log('EditServer queryParams: '
+    );
+    console.log(this.currentRoute.snapshot.queryParams);
+    
+    console.log('EditServer fragment: ' +
+      this.currentRoute.snapshot.fragment
+    );
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
