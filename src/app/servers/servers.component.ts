041f08ba40d825a992201f23670cd850d2314236
jfspps <jfsapps@gmail.com>
2021-01-15 15:46:45 +0000

    navigation with JS methods (e.g. listeners)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -9,10 +10,26 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  // the third parameter is need when a second parameter is passed to navigate() to inject the current path
+  constructor(private serversService: ServersService,
+    private router: Router,
+    private currentPath: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  reloadServers(){
+    console.log('reload servers requested...');
+
+    // relative link; note that navigate() does not know about the current path (unlike routerLink in appComponent)
+    this.router.navigate(['servers']);
+
+    // relative link again, but with the current route/path injected into navigate
+    // (without the second paramter, Angular defaults currentPath to root "/")
+
+    // this would fail because when viewing the servers component, the currentPath is "/servers" and so
+    // navigate() attempts to access "/servers/servers"
+    // this.router.navigate(['servers'], {relativeTo: this.currentPath});
+  }
 }
