178bd61be762259b3c4b9746d05d410039985d8b
jfspps <jfsapps@gmail.com>
2021-01-17 16:12:38 +0000

    storing all routing in a separate module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from "@angular/core";
+
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { ServerComponent } from './servers/server/server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'users', component: UsersComponent, children: [
+  
+      // anything after "users/" will be stored under "args" (can use any literal here) in UserComponent;
+      // note that the path can be executed from any component but the literal argsID and argsName are only handled by UserComponent   
+      { path: ':argsID/:argsName', component: UserComponent}
+    ] },
+    // child components are not loaded by default with <router-outlet> since this is reserved for higher-level, parent routes
+    // see ServersComponent.html for an additional <router-outlet> where the child routes are loaded
+    { path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':argsID/edit', component: EditServerComponent }
+    ] },
+    { path: 'not-found', component: PageNotFoundComponent},
+    // like NodeJS, make sure the generic route below comes last!
+    { path: '**', redirectTo: '/not-found'}
+    // to redirect when using an empty path, use { path: '', redirectTo: '/not-found', pathMatch: 'full' } to avoid permanent redirection
+  ];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        // what is accessible to modules which access this module, appRouting
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,28 +11,8 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-
-    // anything after "users/" will be stored under "args" (can use any literal here) in UserComponent;
-    // note that the path can be executed from any component but the literal argsID and argsName are only handled by UserComponent   
-    { path: ':argsID/:argsName', component: UserComponent}
-  ] },
-  // child components are not loaded by default with <router-outlet> since this is reserved for higher-level, parent routes
-  // see ServersComponent.html for an additional <router-outlet> where the child routes are loaded
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':argsID/edit', component: EditServerComponent }
-  ] },
-  { path: 'not-found', component: PageNotFoundComponent},
-  // like NodeJS, make sure the generic route below comes last!
-  { path: '**', redirectTo: '/not-found'}
-  // to redirect when using an empty path, use { path: '', redirectTo: '/not-found', pathMatch: 'full' } to avoid permanent redirection
-]
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -48,7 +28,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
